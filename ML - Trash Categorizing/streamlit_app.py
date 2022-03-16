cfcfb4c4dbc80b29feb2e48d96ba2c099f51f846
pyramixofficial <balamatiuc2@gmail.com>
2022-03-16 23:35:39 +0200

    almost finished tekwill project

--- ML - Trash Categorizing/streamlit_app.py
+++ ML - Trash Categorizing/streamlit_app.py
@@ -3,11 +3,12 @@ import requests
 import base64
 import io
 from PIL import Image
-import glob
-from base64 import decodebytes
-from io import BytesIO
+import torch
 import numpy as np
-import matplotlib.pyplot as plt
+
+#Import the model and the weights
+model = torch.hub.load(r'D:\My-personal-projects\My-personal-projects\ML - Trash Categorizing\yolov5-master', 'custom', path=r'D:\My-personal-projects\My-personal-projects\ML - Trash Categorizing\yolov5 training weights\second.pt', source = 'local', force_reload=True)
+
 
 ##########
 ##### Set up sidebar.
@@ -19,13 +20,6 @@ st.sidebar.write('#### Select an image to upload.')
 uploaded_file = st.sidebar.file_uploader('',
                                          type=['png', 'jpg', 'jpeg'],
                                          accept_multiple_files=False)
-
-
-## Add in sliders.
-confidence_threshold = st.sidebar.slider('Confidence threshold: What is the minimum acceptable confidence level for displaying a bounding box?', 0.0, 1.0, 0.5, 0.01)
-overlap_threshold = st.sidebar.slider('Overlap threshold: What is the maximum amount of overlap permitted between visible bounding boxes?', 0.0, 1.0, 0.5, 0.01)
-
-
 
 ##########
 ##### Set up main app.
@@ -47,72 +41,14 @@ else:
 ## Subtitle.
 st.write('### Inferenced Image')
 
-# Convert to JPEG Buffer.
-buffered = io.BytesIO()
-image.save(buffered, quality=90, format='JPEG')
+#Processing and saving the image 
+image_result = model(image, size = 640)
+image_result.save(save_dir = r'D:\My-personal-projects\My-personal-projects\ML - Trash Categorizing\results')
 
-# Base 64 encode.
-img_str = base64.b64encode(buffered.getvalue())
-img_str = img_str.decode('ascii')
-
-## Construct the URL to retrieve image.
-upload_url = ''.join([
-    'https://infer.roboflow.com/rf-bccd-bkpj9--1',
-    '?access_token=vbIBKNgIXqAQ',
-    '&format=image',
-    f'&overlap={overlap_threshold * 100}',
-    f'&confidence={confidence_threshold * 100}',
-    '&stroke=2',
-    '&labels=True'
-])
-
-## POST to the API.
-r = requests.post(upload_url,
-                  data=img_str,
-                  headers={
-    'Content-Type': 'application/x-www-form-urlencoded'
-})
-
-image = Image.open(BytesIO(r.content))
-
-# Convert to JPEG Buffer.
-buffered = io.BytesIO()
-image.save(buffered, quality=90, format='JPEG')
+#Opening the saved image
+result_image = Image.open(r'D:\My-personal-projects\My-personal-projects\ML - Trash Categorizing\results\image0.jpg')
 
 # Display image.
-st.image(image,
+st.image(result_image,
+        caption= 'Processed image',
          use_column_width=True)
-
-## Construct the URL to retrieve JSON.
-upload_url = ''.join([
-    'https://infer.roboflow.com/rf-bccd-bkpj9--1',
-    '?access_token=vbIBKNgIXqAQ'
-])
-
-## POST to the API.
-r = requests.post(upload_url,
-                  data=img_str,
-                  headers={
-    'Content-Type': 'application/x-www-form-urlencoded'
-})
-
-## Save the JSON.
-output_dict = r.json()
-
-## Generate list of confidences.
-confidences = [box['confidence'] for box in output_dict['predictions']]
-
-## Summary statistics section in main app.
-st.write('### Summary Statistics')
-st.write(f'Number of Bounding Boxes (ignoring overlap thresholds): {len(confidences)}')
-st.write(f'Average Confidence Level of Bounding Boxes: {(np.round(np.mean(confidences),4))}')
-
-## Histogram in main app.
-st.write('### Histogram of Confidence Levels')
-fig, ax = plt.subplots()
-ax.hist(confidences, bins=10, range=(0.0,1.0))
-st.pyplot(fig)
-
-## Display the JSON in main app.
-st.write('### JSON Output')
-st.write(r.json())
