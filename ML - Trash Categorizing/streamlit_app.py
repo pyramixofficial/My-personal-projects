d7418f7f8755e3618cfa75a078f771b54ed5f75e
pyramixofficial <balamatiuc2@gmail.com>
2022-04-02 23:15:04 +0300

    finished the trash categorizing main functions

--- ML - Trash Categorizing/streamlit_app.py
+++ ML - Trash Categorizing/streamlit_app.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import streamlit as st
 import requests
 import base64
@@ -7,6 +8,7 @@ import torch
 import numpy as np
 import cv2
 from matplotlib import cm
+import pandas as pd
 #Import the model and the weights
 model = torch.hub.load(r"D:\My-personal-projects\My-personal-projects\ML - Trash Categorizing\yolov5-master", 'custom', path=r"D:\My-personal-projects\My-personal-projects\ML - Trash Categorizing\yolov5 training weights\second.pt", source = 'local', force_reload=True)
 
@@ -19,6 +21,8 @@ model = torch.hub.load(r"D:\My-personal-projects\My-personal-projects\ML - Trash
 logo = Image.open(r'D:\My-personal-projects\My-personal-projects\ML - Trash Categorizing\imagees\tekwill_logo.png')
 st.sidebar.image(logo, use_column_width=True)
 st.sidebar.write('#### Select the app mode you want to use.')
+
+#creating the mode choice button
 mode = ["About", "Photo", "Camera detection"]
 choice = st.sidebar.selectbox("Select Activity", mode)
 if choice == "Photo":
@@ -52,7 +56,8 @@ if choice == "Photo":
     #Processing and saving the image 
     image_result = model(image, size = 640)
     image_result.save(save_dir = r'D:\My-personal-projects\My-personal-projects\ML - Trash Categorizing\results')
-    print(help(image_result.render))
+    
+
     #Opening the saved image
     result_image = Image.open(r'D:\My-personal-projects\My-personal-projects\ML - Trash Categorizing\results\image0.jpg')
     
@@ -61,6 +66,18 @@ if choice == "Photo":
             caption= 'Processed image',
             use_column_width=True)
 
+    st.write("### Save the processed data to a csv here:)")
+
+    # Saving the data into a csv
+    if st.button("Save data"):
+        t_np = image_result.__dict__['pred'][0].cpu().numpy() #convert to Numpy array
+        df = pd.DataFrame(t_np, columns=['x_first', 'y_first', 'x_second', 'y_second', 'probability', 'type']) #convert to a dataframe
+        df['type'] = df['type'].apply(lambda x: image_result.__dict__['names'][int(x)])
+        df.to_csv("data.csv") #save to file
+        st.write("Data was saved to data.csv")
+
+
+#Settin the camera detection option
 elif choice == "Camera detection":
     st.header("Live Camera")
     run = st.checkbox('Run')
@@ -71,14 +88,17 @@ elif choice == "Camera detection":
 
 
     while run:
+        #Reading and converting the image
         ret, frame = cam.read()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        #Making predictions on the image and printing it on the app
         frame = model(frame, size=640)
         FRAME_WINDOW.image(frame.render())
+        
     else:
         st.write('Stopped')
 
-
+#The description page of the project
 elif choice == "About":
     st.write("# Tekwill Junior Ambassadors ")
     st.write("### This is a project created by Eduard Balamatiuc and Daria Gheorghieș")
